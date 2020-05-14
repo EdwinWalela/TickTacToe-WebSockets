c5cf52109038f95793d4161828d5d4383285ca4d
edwinwalela <edwinwalela@gmail.com>
2020-05-14 09:15:09 +0300

    update socket URI

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-let socket = io.connect('http://localhost:3000');
+let socket = io.connect('https://xno-sockets.herokuapp.com/');
 $(document).ready(function(){
     let player = $('#player');
     
